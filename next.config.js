a65bb005f2fcc57462dd74f707f79a62f9efc52c
GuillaumeMeheut <guillaume.meheutpro@gmail.com>
2021-11-04 20:30:22 +0100

    a

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   images: {
     loader: "imgix",
-    // path: "https://portfolio-2-self.vercel.app/",
-    path: "http://localhost:3500/",
+    path: "https://portfolio-2-self.vercel.app/",
+    // path: "http://localhost:3500/",
   },
 };
